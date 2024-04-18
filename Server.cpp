7fd15b0a91efa6bf474ddfd6503ff38e01d5a421
Dong Young Kim <dong-kim@c4r10s7.42seoul.kr>
2024-04-18 21:17:03 +0900

    accept

--- Server.cpp
+++ Server.cpp
@@ -26,6 +26,55 @@ Server::Server(const char* _port, const char* _password)
   }
 
   std::cout << "Server listening at " << ::inet_ntoa(serv_addr.sin_addr) << ":" << port << '\n';
+  sockaddr_in client_addr;
+  int client_addr_len, client_fd;
+  struct pollfd fds[100];
+  fds[0].fd = serv_socket;
+  fds[0].events = POLLIN;
+  int num_fds = 1;
+  while (1) {
+    int poll_count = poll(fds, num_fds, -1);
+
+    if (poll_count == -1) {
+        std::cerr << "poll() 함수 실패" << std::endl;
+        break;
+    }
+  
+    // 새로운 연결 수신
+    if (fds[0].revents & POLLIN) {
+        client_addr_len = sizeof(client_addr);
+        client_fd = accept(serv_socket, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
+        if (client_fd == -1) {
+            std::cerr << "연결 수락 실패" << std::endl;
+            continue;
+        }
+
+        if (num_fds < 100 + 1) {
+            fds[num_fds].fd = client_fd;
+            fds[num_fds].events = POLLIN;
+            num_fds++;
+        } else {
+            std::cerr << "클라이언트 제한 초과" << std::endl;
+            close(client_fd);
+        }
+    }
+
+    // 데이터 수신 및 전송
+    char buffer[1024];
+    for (int i = 1; i < num_fds; i++) {
+        if (fds[i].revents & POLLIN) {
+            memset(buffer, 0, 1024);
+            int bytes_received = recv(fds[i].fd, buffer, 1024 - 1, 0);
+            if (bytes_received <= 0) {
+                close(fds[i].fd);
+                fds[i].fd = -1;
+                continue;
+            }
+            std::cout << "수신 데이터: " << buffer;
+            send(fds[i].fd, buffer, bytes_received, 0);
+        }
+    }
+  }
 }
 
 // Server::Server(const std::string& _port, const std::string& _password) {}
--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include "Server.hpp"
 
 int main(int argc, char** argv) {
-  if (argc != 3) {
-    std::cerr << "Usage : " << argv[0] << "s <port> <password>\n";
-    return 1;
-  }
   try {
+    if (argc != 3) {
+      std::string Usage = "Usage : ";
+      std::string ircserv = argv[0];
+      std::string format = " <port> <password>";
+      throw (Error(Usage + ircserv + format));
+    }
     Server serv(argv[1], argv[2]);
   } catch (std::exception& e) {
-    std::cerr << e.what() << '\n';
+    std::cerr << e.what() << std::endl;
     return 1;
   }
 }
