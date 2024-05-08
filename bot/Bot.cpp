020b9b3d9f15747b7d3e8af9f67e1c1932a13dd2
dkael <>
2024-05-08 18:41:34 +0900

    .

---
+++ bot/Bot.cpp
@@ -0,0 +1,277 @@
+#include "Bot.hpp"
+
+Bot::Bot(char** argv) : remain_msg(false) {
+  if (ipv4_chk(argv[1]) == false) {
+    std::cerr << "Invalid ip format!\n";
+    throw std::exception();
+  } else {
+    ipv4 = argv[1];
+  }
+
+  if (port_chk(argv[2]) == false) {
+    std::cerr << "Port range error!\n";
+    throw std::exception();
+  } else {
+    port = std::atoi(argv[2]);
+  }
+
+  password = argv[3];
+  if (ft_strip(password).length() == 0) {
+    std::cerr << "Empty password!\n";
+    throw std::exception();
+  }
+
+  nickname = argv[4];
+  if (('0' <= nickname[0] && nickname[0] <= '9') ||
+      nickname.find_first_of(": \n\t\v\f\r") != std::string::npos) {
+    std::cerr << "Invalid bot nickname!\n";
+    throw std::exception();
+  }
+
+  std::ifstream menu_file_read(argv[5]);
+  if (menu_file_read.is_open() == false) {
+    std::cerr << "Cannot open file!\n";
+    throw std::exception();
+  } else {
+    std::string buf;
+
+    while (getline(menu_file_read, buf)) {
+      if (buf.length() > 0) {
+        menu.push_back(buf);
+      }
+    }
+    menu_file_read.close();
+    std::cerr << "menu size : " << menu.size() << '\n';
+    if (menu.size() == 0) {
+      std::cerr << "Blank menu file!\n";
+      throw std::exception();
+    }
+  }
+}
+
+void Bot::connect_to_serv(void) {
+  bot_sock = ::socket(PF_INET, SOCK_STREAM, 0);
+  if (bot_sock == -1) {
+    perror("socket() error");
+    std::cerr << "errno : " << errno << '\n';
+    throw std::exception();
+  }
+
+  std::memset(&bot_addr, 0, sizeof(bot_addr));
+  bot_addr.sin_family = AF_INET;
+  bot_addr.sin_addr.s_addr = inet_addr(ipv4.c_str());
+  bot_addr.sin_port = htons(port);
+
+  if (::connect(bot_sock, (sockaddr*)&bot_addr, sizeof(bot_addr)) == -1) {
+    perror("connect() error");
+    std::cerr << "errno : " << errno << '\n';
+    throw std::exception();
+  }
+  std::cout << "Bot connected to " << ipv4 << ':' << port << '\n';
+}
+
+void Bot::step_auth(void) {
+  int auth_flag = 0;
+  std::string nick_retry;
+  int nick_retry_cnt = 0;
+  std::vector<std::string> msg_list;
+
+  to_send.push(std::string("PASS ") + password + std::string("\r\n"));
+  to_send.push(std::string("NICK ") + nickname + std::string("\r\n"));
+  to_send.push(std::string("USER ") + nickname + std::string(" 0 * :") +
+               nickname + std::string("\r\n"));
+
+  send_msg_at_queue();
+
+  while (auth_flag != 0x1F) {
+    if (remain_msg == true) {
+      send_msg_at_queue();
+    }
+    try {
+      read_msg_from_socket(bot_sock, msg_list);
+
+      if (msg_list.size() == 0) {
+        sleep(1);
+        continue;
+      }
+      for (int i = 0; i < msg_list.size(); i++) {
+        if (msg_list[i] == std::string("connection finish")) {
+          std::cerr << "Connection lost\n";
+          close(bot_sock);
+          exit(0);
+        }
+        Message msg(bot_sock, msg_list[i]);
+
+        if (msg.get_numeric() == std::string("001")) {
+          auth_flag |= NUMERIC_001;
+        } else if (msg.get_numeric() == std::string("002")) {
+          auth_flag |= NUMERIC_002;
+        } else if (msg.get_numeric() == std::string("003")) {
+          auth_flag |= NUMERIC_003;
+        } else if (msg.get_numeric() == std::string("004")) {
+          auth_flag |= NUMERIC_004;
+          serv_name = msg[1];
+        } else if (msg.get_numeric() == std::string("005")) {
+          auth_flag |= NUMERIC_005;
+        } else if (msg.get_numeric() == std::string("433")) {
+          std::stringstream int_to_str;
+          std::string tmp;
+
+          nick_retry_cnt++;
+          int_to_str << nick_retry_cnt;
+          int_to_str >> tmp;
+          nick_retry = nickname + tmp;
+          to_send.push(std::string("NICK ") + nick_retry + std::string("\r\n"));
+          send_msg_at_queue();
+        } else if (msg.get_numeric() == std::string("421") ||
+                   msg.get_numeric() == std::string("432") ||
+                   msg.get_numeric() == std::string("451") ||
+                   msg.get_numeric() == std::string("462") ||
+                   msg.get_numeric() == std::string("464") ||
+                   msg.get_cmd_type() == ERROR) {
+          std::cerr << msg_list[i];
+          close(bot_sock);
+          exit(1);
+        }
+      }
+    } catch (const std::bad_alloc& e) {
+      std::cerr << e.what() << '\n';
+      std::cerr << "Not enough memory so can't excute vector.push_back "
+                   "or another things require additional memory\n";
+    } catch (const std::length_error& e) {
+      std::cerr << e.what() << '\n';
+      std::cerr << "Maybe index out of range error or std::string is too "
+                   "long to store\n";
+    } catch (const std::exception& e) {
+      // error handling
+      std::cerr << e.what() << '\n';
+      std::cerr << "unexpected exception occur! Program terminated!\n";
+      exit(1);
+    }
+  }
+  if (nick_retry_cnt != 0) {
+    nickname = nick_retry;
+  }
+}
+
+void Bot::step_listen(void) {
+time_t last_ping_chk = time(NULL);
+  time_t ping_send_time = time(NULL);
+  bool is_ping_sent = false;
+  bool is_pong_received = false;
+  std::vector<std::string> msg_list;
+
+  while (true) {
+    if (remain_msg == true) {
+      send_msg_at_queue();
+    }
+    try {
+      read_msg_from_socket(bot_sock, msg_list);
+
+      if (is_ping_sent == false && time(NULL) > last_ping_chk + PING_INTERVAL) {
+        Message ping;
+        ping.set_cmd_type(PING);
+        ping.push_back(serv_name);
+        to_send.push(ping.to_raw_msg());
+        send_msg_at_queue();
+        ping_send_time = time(NULL);
+        is_ping_sent = true;
+        is_pong_received = false;
+      } else if (is_ping_sent == true && is_pong_received == false &&
+                 time(NULL) > ping_send_time + PONG_TIMEOUT) {
+        Message rpl;
+        rpl.set_cmd_type(ERROR);
+        rpl.push_back(":leaving");
+        to_send.push(rpl.to_raw_msg());
+        send_msg_at_queue();
+        close(bot_sock);
+        exit(0);
+      }
+
+      if (msg_list.size() == 0) {
+        sleep(1);
+        continue;
+      }
+      for (int i = 0; i < msg_list.size(); i++) {
+        // std::cerr << "msg " << i << " : " << msg_list[i] << '\n';
+        if (msg_list[i] == std::string("connection finish")) {
+          std::cerr << "Connection lost\n";
+          close(bot_sock);
+          exit(0);
+        }
+        Message msg(bot_sock, msg_list[i]);
+
+        if (msg.get_cmd_type() == PONG) {
+          is_ping_sent = false;
+          is_pong_received = true;
+          last_ping_chk = time(NULL);
+        } else if (msg.get_cmd_type() == PRIVMSG &&
+                   msg.get_params_size() >= 2) {
+          Message rpl;
+          rpl.set_cmd_type(PRIVMSG);
+
+          std::size_t tail = msg.get_source().find("!");
+          std::string who_send = msg.get_source().substr(0, tail);
+          rpl.push_back(who_send);
+          if (msg[1] == std::string("lunch menu recommend")) {
+            int select = std::rand() % menu.size();
+            rpl.push_back(std::string(":Today's lunch menu recommendation : ") +
+                          menu[select]);
+          } else if (ft_upper(msg[1]) == std::string("HELLO") ||
+                     ft_upper(msg[1]) == std::string("HI")) {
+            rpl.push_back(":Hi there");
+          } else {
+            rpl.push_back(":unknown command");
+          }
+          to_send.push(rpl.to_raw_msg());
+        send_msg_at_queue();
+        }
+      }
+    } catch (const std::bad_alloc& e) {
+      std::cerr << e.what() << '\n';
+      std::cerr << "Not enough memory so can't excute vector.push_back "
+                   "or another things require additional memory\n";
+    } catch (const std::length_error& e) {
+      std::cerr << e.what() << '\n';
+      std::cerr << "Maybe index out of range error or std::string is too "
+                   "long to store\n";
+    } catch (const std::exception& e) {
+      // error handling
+      std::cerr << e.what() << '\n';
+      std::cerr << "unexpected exception occur! Program terminated!\n";
+      exit(1);
+    }
+  }
+}
+
+const std::string& Bot::get_ipv4(void) { return ipv4; }
+
+const int Bot::get_port(void) { return port; }
+
+const int Bot::get_bot_sock(void) { return bot_sock; }
+
+const sockaddr_in& Bot::get_bot_adr(void) { return bot_addr; }
+
+const std::string& Bot::get_password(void) { return password; }
+
+const std::string& Bot::get_nickname(void) { return nickname; }
+
+void Bot::send_msg_at_queue(void) {
+  std::size_t to_send_num = to_send.size();
+
+  while (to_send_num > 0) {
+    const std::string& msg_tmp = to_send.front();
+    if (send(bot_sock, msg_tmp.c_str(), msg_tmp.length(), MSG_DONTWAIT) == -1) {
+      if (errno == EWOULDBLOCK) {
+        remain_msg = true;
+        return;
+      } else {
+        std::cerr << "send() error\n";
+        throw std::exception();
+      }
+    }
+    to_send.pop();
+    to_send_num--;
+  }
+  remain_msg = false;
+}
